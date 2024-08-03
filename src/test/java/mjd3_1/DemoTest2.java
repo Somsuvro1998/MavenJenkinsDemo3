fbdb6ac8cdc2cd0adf51f683e7e79516f3a296ea
Somsuvro1998 <Palz.somsuvro@Gmail.com>
2024-08-03 16:57:42 +0530

    1st push

---
+++ src/test/java/mjd3_1/DemoTest2.java
@@ -0,0 +1,22 @@
+package mjd3_1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DemoTest2 {
+	@Test
+	public void demoTest1()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.co.in/");
+		driver.close();
+		
+	}
+
+
+}
